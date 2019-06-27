b856e585a6e76330c51b4eafcc3625761b3291d8
phamtrananh <phamtrananh1088@gmail.com>
2019-06-27 19:17:21 +0900

    async task

--- BeginUnicode/TestUnicode/Form2.Designer.cs
+++ BeginUnicode/TestUnicode/Form2.Designer.cs
@@ -28,79 +28,80 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.cboUnicodeRange = new System.Windows.Forms.ComboBox();
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-			this.btnRunAll = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// cboUnicodeRange
-			// 
-			this.cboUnicodeRange.FormattingEnabled = true;
-			this.cboUnicodeRange.Location = new System.Drawing.Point(13, 13);
-			this.cboUnicodeRange.Margin = new System.Windows.Forms.Padding(4);
-			this.cboUnicodeRange.Name = "cboUnicodeRange";
-			this.cboUnicodeRange.Size = new System.Drawing.Size(220, 20);
-			this.cboUnicodeRange.TabIndex = 6;
-			this.cboUnicodeRange.SelectedIndexChanged += new System.EventHandler(this.cboUnicodeRange_SelectedIndexChanged);
-			// 
-			// panel1
-			// 
-			this.panel1.AutoScroll = true;
-			this.panel1.Location = new System.Drawing.Point(13, 41);
-			this.panel1.Margin = new System.Windows.Forms.Padding(4);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(921, 270);
-			this.panel1.TabIndex = 7;
-			// 
-			// textBox1
-			// 
-			this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-			this.textBox1.Location = new System.Drawing.Point(13, 331);
-			this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(921, 230);
-			this.textBox1.TabIndex = 8;
-			// 
-			// btnRunAll
-			// 
-			this.btnRunAll.Location = new System.Drawing.Point(253, 11);
-			this.btnRunAll.Name = "btnRunAll";
-			this.btnRunAll.Size = new System.Drawing.Size(75, 23);
-			this.btnRunAll.TabIndex = 9;
-			this.btnRunAll.Text = "Run All";
-			this.btnRunAll.UseVisualStyleBackColor = true;
-			this.btnRunAll.Click += new System.EventHandler(this.btnRunAll_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.ForeColor = System.Drawing.Color.Red;
-			this.label1.Location = new System.Drawing.Point(335, 13);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(361, 12);
-			this.label1.TabIndex = 10;
-			this.label1.Text = "When you click on this button, will display the entire region\'s Unicode";
-			// 
-			// Form2
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(949, 617);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.btnRunAll);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.panel1);
-			this.Controls.Add(this.cboUnicodeRange);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.Name = "Form2";
-			this.Text = "Form2";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            this.cboUnicodeRange = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnRunAll = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboUnicodeRange
+            // 
+            this.cboUnicodeRange.FormattingEnabled = true;
+            this.cboUnicodeRange.Location = new System.Drawing.Point(13, 13);
+            this.cboUnicodeRange.Margin = new System.Windows.Forms.Padding(4);
+            this.cboUnicodeRange.Name = "cboUnicodeRange";
+            this.cboUnicodeRange.Size = new System.Drawing.Size(220, 20);
+            this.cboUnicodeRange.TabIndex = 6;
+            this.cboUnicodeRange.SelectedIndexChanged += new System.EventHandler(this.cboUnicodeRange_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(13, 41);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(921, 270);
+            this.panel1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(13, 331);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(921, 230);
+            this.textBox1.TabIndex = 8;
+            // 
+            // btnRunAll
+            // 
+            this.btnRunAll.Location = new System.Drawing.Point(253, 11);
+            this.btnRunAll.Name = "btnRunAll";
+            this.btnRunAll.Size = new System.Drawing.Size(75, 23);
+            this.btnRunAll.TabIndex = 9;
+            this.btnRunAll.Text = "Run All";
+            this.btnRunAll.UseVisualStyleBackColor = true;
+            this.btnRunAll.Click += new System.EventHandler(this.btnRunAll_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(335, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "When you click on this button, will display the entire region\'s Unicode";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 617);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRunAll);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cboUnicodeRange);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
--- BeginUnicode/TestUnicode/Form2.cs
+++ BeginUnicode/TestUnicode/Form2.cs
@@ -13,187 +13,227 @@ using System.Windows.Forms;
 
 namespace Anh.TestUnicode
 {
-	public partial class Form2 : Form
-	{
-		public Form2()
-		{
-			InitializeComponent();
-			Task.Factory.StartNew(Initialize2);
-		}
+    public partial class Form2 : Form
+    {
+        #region Field
+        bool _RenderButton = true;
+        #endregion
 
-		private async void Initialize2()
-		{
-			UniCodeRange[] array1D = await XmlUnicodeRange.Main();
-			BeginInvoke(new Action(() =>
-			{
-				cboUnicodeRange.Items.AddRange(array1D);
-				cboUnicodeRange.ValueMember = "Value";
-				cboUnicodeRange.DisplayMember = "Name";
-			}));
-		}
+        #region Constructor
+        public Form2()
+        {
+            InitializeComponent();
+            Task.Factory.StartNew(Initialize2);
+        }
+        #endregion
 
-		private void cboUnicodeRange_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			UniCodeRange range = cboUnicodeRange.SelectedItem as UniCodeRange;
-			UnicodeRangeSelected(range);
-		}
+        #region Initialize
+        private async Task Initialize2()
+        {
+            UniCodeRange[] array1D = await XmlUnicodeRange.Main();
+            BeginInvoke(new Action(() =>
+            {
+                //cboUnicodeRange.Items.AddRange(array1D.Take(3).ToArray());
+                cboUnicodeRange.Items.AddRange(array1D);
+                cboUnicodeRange.ValueMember = "Value";
+                cboUnicodeRange.DisplayMember = "Name";
+            }));
+        }
+        #endregion
 
-		private void UnicodeRangeSelected(UniCodeRange range)
-		{
-			string fileName = string.Format(@".\{0}.xml", range.Value);
-			if (File.Exists(fileName))
-			{
-				string source = "";
-				using (FileStream fs = System.IO.File.Open(fileName, System.IO.FileMode.Open))
-				{
-					Byte[] bytes = new Byte[fs.Length];
-					fs.Read(bytes, 0, (int)fs.Length);
-					source = new UnicodeEncoding().GetString(bytes);
-				}
-				object kk = new object[] { source, range };
-				Task.Factory.StartNew((s) =>
-				{
-					LoadUniCode(s);
-				}, kk);
-			}
-			else
-			{
-				string url = range.Href;
-				HtmlDocument doc = HtmlSourceCode.Code(url);
-				string source = HtmlSourceCode.FindCode(doc);
-				object kk = new object[] { source, range };
-				Task.Factory.StartNew((s) =>
-				{
-					LoadUniCode(s);
-				}, kk);
-			}
+        #region Bussiness
+        private async Task<string> UnicodeRangeSelected(UniCodeRange range)
+        {
+            string fileName = string.Format(@".\{0}.xml", range.Value);
+            if (File.Exists(fileName))
+            {
+                Task<string> t = Task.Run<string>(() =>
+                {
+                    string source = "";
+                    using (FileStream fs = System.IO.File.Open(fileName, System.IO.FileMode.Open))
+                    {
+                        Byte[] bytes = new Byte[fs.Length];
+                        fs.Read(bytes, 0, (int)fs.Length);
+                        source = new UnicodeEncoding().GetString(bytes);
+                    }
+                    return source;
+                });
+                string res = await t;
+                object kk = new object[] { res, range };
+                await LoadUniCode(kk);
 
-		}
-		private async void LoadUniCode(object obj)
-		{
-			object[] arObj = obj as object[];
-			string source = (string)arObj[0];
-			UniCodeRange range = arObj[1] as UniCodeRange;
+            }
+            else
+            {
+                string url = range.Href;
+                string source = await HtmlSourceCode.Code(url);
+                object kk = new object[] { source, range };
+                await LoadUniCode(kk);
+            }
+            return range.Value;
+        }
+        private async Task LoadUniCode(object obj)
+        {
+            object[] arObj = obj as object[];
+            string source = (string)arObj[0];
+            UniCodeRange range = arObj[1] as UniCodeRange;
 
-			string fileName = string.Format(@".\{0}.xml", range.Value);
-			UnicodeData[] array1D = null;
-			if (!File.Exists(fileName))
-			{
+            string fileName = string.Format(@".\{0}.xml", range.Value);
+            UnicodeData[] array1D = null;
+            if (!File.Exists(fileName))
+            {
+                array1D = await XmlUnicodeRegion.Main(source, true);
+                if (_RenderButton) BeginInvoke(CreatButtons(array1D));
+                await XmlUnicodeRegion.SaveXml(array1D, fileName);
+                if (AppSetting.IsOpenFileGegionUnicodeData)
+                {
+                    System.Diagnostics.Process.Start("notepad++.exe", fileName);
+                }
+                ;
+            }
+            else
+            {
+                array1D = await XmlUnicodeRegion.Main(fileName, false);
+                if (_RenderButton) BeginInvoke(CreatButtons(array1D));
+            }
+        }
 
-				array1D = await XmlUnicodeRegion.Main(source, true);
-				BeginInvoke(CreatButtons(array1D));
-				XmlUnicodeRegion.SaveXml(array1D, fileName);
-				if (AppSetting.IsOpenFileGegionUnicodeData)
-				{
-					System.Diagnostics.Process.Start("notepad++.exe", fileName);
-				}
-				;
-			}
-			else
-			{
-				array1D = await XmlUnicodeRegion.Main(fileName, false);
-				BeginInvoke(CreatButtons(array1D));
-			}
-		}
+        Action CreatButtons(UnicodeData[] array1D)
+        {
+            var act = new Action(() =>
+            {
+                this.panel1.Controls.Clear();
+                int tabIndex = 0;
+                for (int i = 0, j = 0, k = 0; i < 1; i++)
+                {
+                    for (int l = 0; l < array1D.Length; l++)
+                    {
+                        UnicodeData d = array1D[l];
+                        Button butt = new Button();
+                        // 
+                        // button
+                        // 
+                        butt.Location = new System.Drawing.Point(3 + j * 40, 3 + k * 40);
+                        butt.Name = "btnClear";
+                        butt.Size = new System.Drawing.Size(32, 27);
+                        butt.TabIndex = i * 17 + l;
+                        butt.Text = d.Name;
+                        butt.Tag = d;
+                        butt.UseVisualStyleBackColor = true;
+                        butt.Click += new System.EventHandler(this.btnUnicode_Click);
+                        if (d.IsLeaf && !d.Inactive)
+                        {
+                            this.toolTip1.SetToolTip(butt, d.Title);
+                        }
+                        else
+                        {
+                            butt.Enabled = false;
+                        }
+                        this.panel1.Controls.Add(butt);
+                        j++;
+                        if (j == 17)
+                        {
+                            j = 0;
+                            k++;
+                        }
+                        tabIndex = butt.TabIndex;
+                    }
+                }
+                SetTabIndex(tabIndex, new Control[] { textBox1 });
+            });
+            return act;
+        }
 
-		Action CreatButtons(UnicodeData[] array1D)
-		{
-			var act = new Action(() =>
-			{
-				this.panel1.Controls.Clear();
-				int tabIndex = 0;
-				for (int i = 0, j = 0, k = 0; i < 1; i++)
-				{
-					for (int l = 0; l < array1D.Length; l++)
-					{
-						UnicodeData d = array1D[l];
-						Button butt = new Button();
-						// 
-						// button
-						// 
-						butt.Location = new System.Drawing.Point(3 + j * 40, 3 + k * 40);
-						butt.Name = "btnClear";
-						butt.Size = new System.Drawing.Size(32, 27);
-						butt.TabIndex = i * 17 + l;
-						butt.Text = d.Name;
-						butt.Tag = d;
-						butt.UseVisualStyleBackColor = true;
-						butt.Click += new System.EventHandler(this.btnUnicode_Click);
-						if (d.IsLeaf && !d.Inactive)
-						{
-							this.toolTip1.SetToolTip(butt, d.Title);
-						}
-						else
-						{
-							butt.Enabled = false;
-						}
-						this.panel1.Controls.Add(butt);
-						j++;
-						if (j == 17)
-						{
-							j = 0;
-							k++;
-						}
-						tabIndex = butt.TabIndex;
-					}
-				}
-				SetTabIndex(tabIndex, new Control[] { textBox1 });
-			});
-			return act;
-		}
+        private void SetTabIndex(int currentTabIndex, Control[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].TabIndex = currentTabIndex + 1 + i;
+            }
+        }
+        private async Task RunAll()
+        {
+            if (MessageBox.Show("When you click on this button, will display the entire region's Unicode. And it will take some time. Do you want to do it?", "Confirm", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                List<UniCodeRange> liAll = new List<UniCodeRange>();
+                List<Task<string>> tasks = new List<Task<string>>();
+                for (int i = 0; i < cboUnicodeRange.Items.Count; i++)
+                {
+                    UniCodeRange range = cboUnicodeRange.Items[i] as UniCodeRange;
+                    liAll.Add(cboUnicodeRange.Items[i] as UniCodeRange);
 
-		private void btnUnicode_Click(object sender, EventArgs e)
-		{
-			Button butt = sender as Button;
-			UnicodeData d = butt.Tag as UnicodeData;
-			int c = Convert.ToInt32("0x" + d.Code, 16);
-			String s = ((char)c).ToString();
-			textBox1.AppendText(s);
-		}
+                }
+                while (liAll.Count > 0)
+                {
+                    foreach (var item in liAll.Take(5))
+                    {
+                        tasks.Add(UnicodeRangeSelected(item));
+                    }
+                    liAll.RemoveRange(0, Math.Min(5, liAll.Count));
+                    Task<string[]> taskFinish = Task.WhenAll<string>(tasks);
+                    string[] fileName = await taskFinish;
+                    foreach (var item in fileName)
+                    {
+                        textBox1.AppendText(item + " success" + Environment.NewLine);
+                    }
+                    //if (taskFinish.Status == TaskStatus.RanToCompletion)
+                    //{
+                    //}
 
-		private void SetTabIndex(int currentTabIndex, Control[] array)
-		{
-			for (int i = 0; i < array.Length; i++)
-			{
-				array[i].TabIndex = currentTabIndex + 1 + i;
-			}
-		}
+                }
+                BeginInvoke(new Action(() =>
+                {
+                    cboUnicodeRange.SelectedValue = "cjk-unified-ideographs";
+                }));
 
-		private async void btnRunAll_Click(object sender, EventArgs e)
-		{
-			if (MessageBox.Show("When you click on this button, will display the entire region's Unicode. And it will take some time. Do you want to do it?", "Confirm", MessageBoxButtons.OKCancel) == DialogResult.OK)
-			{
-				int failed = 0;
-				Task[] tasks = new Task[cboUnicodeRange.Items.Count];
-				for (int i = 0; i < cboUnicodeRange.Items.Count; i++)
-				{
-					UniCodeRange range = cboUnicodeRange.Items[i] as UniCodeRange;
-					tasks[i] = new Task((p) =>
-					{
-						try
-						{
-							UnicodeRangeSelected(p as UniCodeRange);
-						}
-						catch (Exception)
-						{
-							Interlocked.Increment(ref failed);
-						}
-					}, range);
-					tasks[i].Start();
-				}
-				Task taskFinish = Task.WhenAll(tasks);
-				try
-				{
-					await taskFinish;
-				}
-				catch { }
 
-				if (taskFinish.Status == TaskStatus.RanToCompletion)
-					MessageBox.Show("Finished");
-				else if (taskFinish.Status == TaskStatus.Faulted)
-					Console.WriteLine("{0} region's Unicode failed", failed);
-			}
-		}
+            }
+        }
+        #endregion
 
-	}
+        #region EVENTS
+        #region btnRunAll_Click
+        private async void btnRunAll_Click(object sender, EventArgs e)
+        {
+            _RenderButton = false;
+            try
+            {
+                await RunAll();
+            }
+            catch (System.Net.WebException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        #endregion
+
+        #region cboUnicodeRange_SelectedIndexChanged
+        private async void cboUnicodeRange_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _RenderButton = true;
+            UniCodeRange range = cboUnicodeRange.SelectedItem as UniCodeRange;
+            try
+            {
+                await UnicodeRangeSelected(range);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+        #endregion
+
+        #region btnUnicode_Click
+        private void btnUnicode_Click(object sender, EventArgs e)
+        {
+            Button butt = sender as Button;
+            UnicodeData d = butt.Tag as UnicodeData;
+            int c = Convert.ToInt32("0x" + d.Code, 16);
+            String s = ((char)c).ToString();
+            textBox1.AppendText(s);
+        }
+        #endregion
+        #endregion
+    }
 }

49789ae9205b5613f2483773443f67f212538cbb
phamtrananh <phamtrananh1088@gmail.com>
2019-08-14 15:11:01 +0900

    master: TakeEncodingFile

---
+++ TakeEncodingFile/TakeEncodingFile/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace TakeEncodingFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnTakeEndCoding = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnTakeEndCoding
+            // 
+            this.btnTakeEndCoding.Location = new System.Drawing.Point(485, 3);
+            this.btnTakeEndCoding.Name = "btnTakeEndCoding";
+            this.btnTakeEndCoding.Size = new System.Drawing.Size(95, 23);
+            this.btnTakeEndCoding.TabIndex = 2;
+            this.btnTakeEndCoding.Text = "Lấy Encoding";
+            this.btnTakeEndCoding.UseVisualStyleBackColor = true;
+            this.btnTakeEndCoding.Click += new System.EventHandler(this.btnTakeEndCoding_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(346, 19);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên tệp";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 35);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(346, 94);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Encode";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(485, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Chuyển";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(123, 135);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(346, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Chuyển sang Encode";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 117F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnTakeEndCoding, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(24, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(599, 202);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 258);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Take Encoding File";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnTakeEndCoding;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ TakeEncodingFile/TakeEncodingFile/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TakeEncodingFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Encode", typeof(string));
+            dt.Columns.Add("CodePage", typeof(string));
+            dt.Columns.Add("Desc", typeof(string));
+
+            dt.Rows.Add("Shift-JIS", "932", "Japanese (Shift-JIS) - Codepage 932");
+            comboBox1.DataSource = dt;
+            comboBox1.ValueMember = "CodePage";
+            comboBox1.DisplayMember = "Desc";
+        }
+
+        private void btnTakeEndCoding_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Hãy chọn một tệp");
+                textBox1.Focus();
+                return;
+            }
+            Encoding code = Form1.GetEncoding(textBox1.Text.Trim());
+            string codePage = Form1.GetCharset(textBox1.Text.Trim());
+            textBox2.Text = code.ToString() + " " + codePage + Environment.NewLine + Form1.GetEncoding1(textBox1.Text.Trim()) + " " + codePage;
+            textBox1.Tag = new EndData() { Encoding = code, Encode = code.BodyName, CodePage = codePage, Desc = code.BodyName + " " + codePage };
+        }
+        private class EndData
+        {
+            public Encoding Encoding;
+            public string Encode;
+            public string CodePage;
+            public string Desc;
+        }
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
+        /// Defaults to ASCII when detection of the text file's endianness fails.
+        /// </summary>
+        /// <param name="filename">The text file to analyze.</param>
+        /// <returns>The detected encoding.</returns>
+        public static Encoding GetEncoding(string filename)
+        {
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
+            return Encoding.ASCII;
+        }
+
+        public static Encoding GetEncoding1(string fileName)
+        {
+            using (var reader = new StreamReader(fileName, Encoding.ASCII, true))
+            {
+                reader.Peek(); // you need this!
+                var encoding = reader.CurrentEncoding;
+                return encoding;
+            }
+        }
+
+        public static string GetCharset(string fileName)
+        {
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                Ude.CharsetDetector cdet = new Ude.CharsetDetector();
+                cdet.Feed(fs);
+                cdet.DataEnd();
+                if (cdet.Charset != null)
+                {
+                    return cdet.Charset;
+                }
+                return "";
+            }
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Tag == null)
+            {
+                MessageBox.Show("Chưa xác định Encoding");
+                btnTakeEndCoding.Focus();
+                return;
+            }
+            if (comboBox1.SelectedIndex < 0)
+            {
+                MessageBox.Show("Chưa chọn Encoding");
+                comboBox1.Focus();
+                return;
+            }
+            EndData data = textBox1.Tag as EndData;
+            FileInfo FileInfo = new FileInfo(textBox1.Text.Trim());
+
+            int codePageNew = -1;
+            Encoding EncodingNew;
+            Encoding EncodingOld;
+            if (int.TryParse(comboBox1.SelectedValue.ToString(), out codePageNew))
+            {
+                EncodingNew = Encoding.GetEncoding(codePageNew);
+            }
+            else
+            {
+                string codeNew = ((DataRowView)comboBox1.SelectedItem).Row["Encode"].ToString();
+                EncodingNew = Encoding.GetEncoding(codeNew);
+            }
+            if (data.CodePage.Length == 0)
+            {
+                EncodingOld = data.Encoding;
+            }
+            else
+            {
+                EncodingOld = Encoding.GetEncoding(data.CodePage);
+            }
+
+            if (EncodingOld.Equals(EncodingNew))
+            {
+                MessageBox.Show("It is same Encoding");
+                return;
+            }
+            var stringFromFile = "";
+
+            using (var reader = new StreamReader(textBox1.Text.Trim(), EncodingOld))
+            {
+                stringFromFile = reader.ReadToEnd();
+            }
+
+            using (var writer = new StreamWriter(Path.Combine(Application.StartupPath, FileInfo.Name),false, EncodingNew))
+            {
+                //read only first line
+                writer.Write(stringFromFile);
+            }
+            
+            System.Diagnostics.Process.Start(Application.StartupPath);
+
+        }
+    }
+}
